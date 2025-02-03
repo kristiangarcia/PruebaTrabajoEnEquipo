9eb733034ac4a795dfc940786ddb07f7361ba6c8
Kristiansito <kristianpro498@gmail.com>
2025-02-03 08:52:27 +0100

    Añadido stub clase coche

---
+++ src/Coche.java
@@ -0,0 +1,11 @@
+public class Coche {
+    public Coche(int matricula){
+        throw new UnsupportedOperationException("sin programar");
+    }
+    public void arrancar() {
+        throw new UnsupportedOperationException("sin programar");
+    }
+    public void detener(){
+        throw new UnsupportedOperationException("sin programar");
+    }
+}
